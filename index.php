466c4db38cb9d68a6210684a07fc30168a2abcb3
henryvalle <57874459+henryvalle@users.noreply.github.com>
2020-11-17 12:36:24 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,8 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hola muundo-  Microservicios Esta es mi primera pagina con PHP usando Jenkins y OpenShift Online</p>'; ?> 
+ </body>
+</html>
---
+++ php47/index.php
@@ -0,0 +1,8 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p> Hola PHP47 demo mejora 3</p>'; ?> 
+ </body>
+</html>
